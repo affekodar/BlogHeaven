97648f92b49ebb009e5a0d1c094778056e2c70ee
affekodar <alfons.fifa@gmail.com>
2024-06-04 12:18:59 +0200

    almost done every request works

--- src/main/java/com/example/blogheaven/services/UserServiceInterface.java
+++ src/main/java/com/example/blogheaven/services/UserServiceInterface.java
@@ -3,7 +3,6 @@ package com.example.blogheaven.services;
 import com.example.blogheaven.entities.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserServiceInterface {
     List<User> fetchAllUsers();
